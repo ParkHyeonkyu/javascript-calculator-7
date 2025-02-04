0994106f898e58795540f910a8f2f584d8231ae1
ParkHyeonkyu <162525426+ParkHyeonkyu@users.noreply.github.com>
2025-02-04 23:40:43 +0900

    feat: 문자열 입력 요구기능 구현
    Console을 활용하여 사용자에게 문자열을 입력받는 기능 구현
    commit 테스트

--- src/App.js
+++ src/App.js
@@ -1,5 +1,10 @@
+import { Console } from "@woowacourse/mission-utils";
+
 class App {
-  async run() {}
+  async run() {
+    const userInput = await Console.readLineAsync(`덧셈할 문자열을 입력해주세요.\n`);
+    Console.print("값: " + userInput);
+  }
 }
 
 export default App;
