0d424273253938663ed4825555efbf7c56924981
ParkHyeonkyu <162525426+ParkHyeonkyu@users.noreply.github.com>
2025-02-13 23:20:30 +0900

    refactor: 피드백 반영 리팩토링
    깃허브 코드리뷰의 피드백을 반영한 리팩토링 코드입니당

--- src/App.js
+++ src/App.js
@@ -3,49 +3,51 @@ import { Console } from "@woowacourse/mission-utils";
 class App {
   async run() {
     const sumResult = async (input) => {
-      
-        input = input.replace(/\\n/g, "\n");
+      input = input.replace(/\\n/g, "\n");
 
-        let delimiter = /[,:]/;
-        const escapeRegExp = (str) =>
-          str.replace(/[-\/\\^$*+?.()|[\]{}]/g, "\\$&");
+      let delimiter = /[,:]/;
+      const escapeRegExp = (str) =>
+        str.replace(/[-\/\\^$*+?.()|[\]{}]/g, "\\$&");
 
-        if (input.startsWith("//")) {
-          const delimiterEndIndex = input.search(/\n/);
+      if (input.startsWith("//")) {
+        const delimiterEndIndex = input.search(/\n/);
 
-          if (delimiterEndIndex === -1) {
-            throw new Error("\\n이 필요합니다.");
-          }
-
-          let customDelimiter = input.slice(2, delimiterEndIndex);
-          if (/(.)\1+/.test(customDelimiter)) {
-            throw new Error("중복된 구분자를 사용할 수 없습니다.");
-          }
-          if (customDelimiter === ".") {
-            throw new Error(".은 구분자로 사용할 수 없습니다.");
-          }
-          input = input.slice(delimiterEndIndex + 1);
-
-          delimiter = new RegExp(`[,:${escapeRegExp(customDelimiter)}]`);
-        } else if (!(input.startsWith("//") || !isNaN(Number(input[0])))) {
-          throw new Error("양수 또는 //로 시작해야 합니다.");
+        if (delimiterEndIndex === -1) {
+          throw new Error("\\n이 필요합니다.");
         }
 
-        const inputArr = input.split(delimiter);
+        let customDelimiter = input.slice(2, delimiterEndIndex);
+        if (/(.)\1+/.test(customDelimiter)) {
+          throw new Error("중복된 구분자를 사용할 수 없습니다.");
+        }
+        // if (customDelimiter === ".") {
+        //   throw new Error(".은 구분자로 사용할 수 없습니다.");
+        // }
+        input = input.slice(delimiterEndIndex + 1);
 
-        const sum = inputArr.reduce((acc, cur) => {
-          const num = Number(cur);
-          if (!isNaN(num)) {
-            if (num < 0) {
-              throw new Error("음수를 입력할 수 없습니다.");
-            }
-            return acc + num;
+        delimiter = new RegExp(`[,:${escapeRegExp(customDelimiter)}]`);
+      } else if (!(input.startsWith("//") || !isNaN(Number(input[0])))) {
+        throw new Error("양수 또는 //로 시작해야 합니다.");
+      }
+
+      const inputArr = input.split(delimiter);
+
+      const sum = inputArr.reduce((acc, cur) => {
+        const num = Number(cur);
+        if (!isNaN(num)) {
+          if (num < 0) {
+            throw new Error("음수를 입력할 수 없습니다.");
           }
+          else if (!Number.isInteger(num)){
+            throw new Error("정수만 입력 가능합니다.")
+          }
+          return acc + num;
+        } else {
           throw new Error("유효하지 않은 문자가 포함되어있습니다.");
-        }, 0);
+        }
+      }, 0);
 
-        return sum;
-      
+      return sum.toFixed(1);
     };
 
     try {
